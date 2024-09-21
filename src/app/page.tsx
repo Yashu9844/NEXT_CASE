63fd5d9514ba8d31dd4e1264a751dcb51c58c1bf
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-09-21 13:14:10 +0530

    done with Phone componet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import MaxWidthWrapper from "./components/MaxWidthWrapper";
-import { Check, Star } from "lucide-react";
+import { Check,  Star } from "lucide-react";
+import Phone from "./components/Phone";
 
 
 export default function Home() {
@@ -69,7 +70,13 @@ export default function Home() {
               </div>
 
             </div>
- 
+             <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+              <div className="relative md:max-w-xl">
+                <img className="absolute lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" src="/your-image.png" alt="image" />
+                <img  className="absolute w-20 -left-6 -bottom-6 select-none" src="/line.png" alt="image" />
+                <Phone  className="w-64" imgSrc="/testimonials/1.jpg" />
+              </div>
+             </div>
               </MaxWidthWrapper>
             </section>
            </div>
