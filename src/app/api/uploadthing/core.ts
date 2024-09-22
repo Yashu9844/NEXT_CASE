d0f1fd16294dfbfb3269cacf17ccced158b8d248
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-09-23 01:34:52 +0530

    add image/next-image';

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,61 +1,52 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { z } from 'zod';
-import sharp from 'sharp'
+import { z } from "zod";
+import sharp from "sharp";
 import { db } from "@/db";
 const f = createUploadthing();
- 
-const auth = (req: Request) => ({ id: "fakeId" }); 
- 
+
+const auth = (req: Request) => ({ id: "fakeId" });
 
 export const ourFileRouter = {
-
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .input(z.object({configId:z.string().optional()}))
+    .input(z.object({ configId: z.string().optional() }))
     .middleware(async ({ input }) => {
-     
-     return {input}
- 
-    
-    
+      return { input };
     })
+
     .onUploadComplete(async ({ metadata, file }) => {
-     const {configId} = metadata.input
-      
-const res = await fetch(file.url)
-const buffer = await res.arrayBuffer()
+      const { configId } = metadata.input;
 
-const imgMetadata = await sharp(buffer).metadata();
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
 
-const {width,height} = imgMetadata
+      const imgMetadata = await sharp(buffer).metadata();
 
-if(!configId){
-  const configuration = await db.configuration.create({
-    data : {
-      imageUrl:file.url,
-      height:height || 500,
-      width: width || 500
-    },
+      const { width, height } = imgMetadata;
 
-    
-  })
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
 
-  return { configId: configuration.id};
+        return { configId: configuration.id };
+      } else {
+        const updateConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+        return { configId: updateConfiguration.id };
+      }
 
-}else{
-  const updateConfiguration = await db.configuration.update({
-    where:{
-      id: configId
-    },
-    data:{
-      croppedImageUrl:file.url,
-    }
-  })
-return {configId :updateConfiguration.id}
-}
-
-
-     
+      return { configId };
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,9 +1,34 @@
-const Page = async () => {
-  return (
-    <div>
-      Page
-    </div>
-  );
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
+interface PageProps{
+    searchParams:{
+        [key:string] : string | string[] |  undefined  
+    }
+}
+
+const Page = async ({searchParams} : PageProps) => {
+const {id} = searchParams
+
+if(!id || typeof id !== 'string'){
+ return notFound()
+}
+
+
+const configuration = await db.configuration.findUnique({
+    where: {id},
+})
+
+if(!configuration){
+    return notFound()  // If no configuration found, return a 404 page
+ 
+}
+
+const {imageUrl , width , height} = configuration
+
+
+  return <DesignConfigurator/>
 };
 
 export default Page;
