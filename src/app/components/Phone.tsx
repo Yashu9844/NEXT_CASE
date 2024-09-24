7eaa11810fce72f42151ad2e5c75cc93c9ffe7b9
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-09-24 15:35:46 +0530

    add preview page

--- src/app/components/Phone.tsx
+++ src/app/components/Phone.tsx
@@ -7,13 +7,15 @@ interface PhoneProps extends HTMLAttributes<HTMLDivElement>{
 }
 
 const Phone = ({imgSrc,className,dark=false,...props}:PhoneProps) => {
+
+  
   return (
     <div className={cn('relative pointer-events-none z-50 overflow-hidden',
         className
     )} {...props}>
      <img className="pointer-events-none z-50 select-none" src={dark ? '/phone-template-dark-edges.png': '/phone-template-white-edges.png '} alt="Phone image" />
      <div className="absolute -z-10 inset-0">
-        <img className="object-cover" src={imgSrc} alt="image" />
+        <img className="object-cover min-w-full  min-h-full " src={imgSrc} alt="image" />
      </div>
     </div>
   );
---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,40 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignPreview from "./DesignPreview";
+
+interface PageProps{
+    searchParams:{
+        [key:string]:string | string[] | undefined; //id : value
+    }
+}
+
+const Page = async  ({searchParams}:PageProps) => {
+
+ const {id} =searchParams
+
+ if(!id || typeof id !== "string") {
+    return notFound()
+ }
+
+const configuration = await db.configuration.findUnique({
+    where: { id },
+  
+})
+
+if(!configuration){
+    return notFound()  // If no configuration found, return a 404 page
+ 
+}
+
+return  <DesignPreview configuration={configuration} />
+
+
+
+  return (
+    <div>
+      Page
+    </div>
+  );
+};
+
+export default Page;
