56959982d20281c3b9b8a98130faa0d43661ed2a
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-09-25 23:16:10 +0530

    test commit

--- src/app/configure/preview/page.tsx
+++ src/app/configure/preview/page.tsx
@@ -10,7 +10,7 @@ interface PageProps{
 
 const Page = async  ({searchParams}:PageProps) => {
 
- const {id} =searchParams
+ const {id}  = searchParams
 
  if(!id || typeof id !== "string") {
     return notFound()
