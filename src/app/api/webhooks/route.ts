10fd42836007a5328330ed15506a92765e1449f5
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-10-04 00:17:00 +0530

    error is solved to deployment

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -51,7 +51,7 @@ export async function POST(req:Request){
                   }
                 }
             }
-          }),
+          })
           await  db.order.update({
             where:{id:orderId},
             data:{
