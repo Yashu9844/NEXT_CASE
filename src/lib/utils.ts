404215d4fefed783d3bed56deaf65c75a138144d
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-10-06 23:11:03 +0530

    add descriptive metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -13,4 +14,47 @@ export const formatPrice = (price :number) => {
     maximumFractionDigits: 2,
   })
   return formatter.format(price)
+}
+
+export function constructMetaData ({
+  title = "CassyCase - custom high-quality phone cases",
+  description = "A customizable and affordable phone case made from high-quality materials",
+  icons = '/favicon.ico',
+  image = '/thumbnail.png',
+}:{
+  title?: string,
+  description?: string,
+  icons?: string,
+  image?: string,
+} = {}): Metadata{
+  return {
+    title,
+    description,
+    openGraph: {
+      type: "website",
+      url: "https://cassycase.com",
+      title,
+      description,
+      images: [
+        {
+          url: image,
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+      
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images:[
+        image
+      ],
+
+    },
+    icons
+   
+  }
 }
